735578ea3a1e47adfc54b2a3358857e2d157b618
Ryan LeFevre <meltingice8917@gmail.com>
2009-10-18 02:17:13 -0400

    Some code restructuring

--- jSearch.js
+++ jSearch.js
@@ -2,10 +2,12 @@
  * jSearch by MeltingIce and Jarques
  * jQuery page search & highlight plugin
  */
- 
+
+(function($){
+
 $.fn.jSearch = function(query,customOptions){
 	var options = $.extend({},$.fn.jSearch.defaultOptions,customOptions);
-	
+	new jSearch($(this),query,options);
 }
 
 $.fn.jSearch.defaultOptions = {
@@ -14,3 +16,25 @@ $.fn.jSearch.defaultOptions = {
 	'engine':'literal',
 	'recursive':false
 }
+
+function jSearch(input,query,options){
+	this.input = input;
+	this.query = query;
+	this.options = options;
+	
+	this.init();
+}
+
+jSearch.prototype.init = function(){
+	this.html = this.input.html();
+	
+	if(this.options.engine == 'literal'){
+		this.literalSearch();
+	}
+}
+
+jSearch.prototype.literalSearch = function(){
+	
+}
+
+})(jQuery);
